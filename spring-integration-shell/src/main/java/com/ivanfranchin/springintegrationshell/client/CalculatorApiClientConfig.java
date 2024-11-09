63dc47f305a5ad75353a4e8da94e57fa9207ca8b
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-09 08:58:30 +0100

    Replace RestTemplate with RestClient
    - Using an auto-configured WebClient.Builder for trace propagation when needed;
    - Remove httpclient5 dependency in spring-integration-shell pom.xml as it is not needed anymore.

--- spring-integration-shell/src/main/java/com/ivanfranchin/springintegrationshell/client/CalculatorApiClientConfig.java
+++ spring-integration-shell/src/main/java/com/ivanfranchin/springintegrationshell/client/CalculatorApiClientConfig.java
@@ -3,10 +3,9 @@ package com.ivanfranchin.springintegrationshell.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.support.RestTemplateAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class CalculatorApiClientConfig {
@@ -15,10 +14,9 @@ public class CalculatorApiClientConfig {
     private String calculatorApiUrl;
 
     @Bean
-    public CalculatorApiClient calculatorApiClient() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(calculatorApiUrl));
-        RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
+    public CalculatorApiClient calculatorApiClient(RestClient.Builder builder) {
+        RestClient restClient = builder.baseUrl(calculatorApiUrl).build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(CalculatorApiClient.class);
     }
--- spring-integration-shell/src/main/java/com/ivanfranchin/springintegrationshell/client/FileServiceClientConfig.java
+++ spring-integration-shell/src/main/java/com/ivanfranchin/springintegrationshell/client/FileServiceClientConfig.java
@@ -3,10 +3,9 @@ package com.ivanfranchin.springintegrationshell.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.client.support.RestTemplateAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class FileServiceClientConfig {
@@ -15,10 +14,9 @@ public class FileServiceClientConfig {
     private String fileServiceUrl;
 
     @Bean
-    public FileServiceClient fileServiceClient() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(fileServiceUrl));
-        RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
+    public FileServiceClient fileServiceClient(RestClient.Builder builder) {
+        RestClient restClient = builder.baseUrl(fileServiceUrl).build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(FileServiceClient.class);
     }
